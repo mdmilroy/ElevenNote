8018288d46869a6439eb5a82be84c3cc476bdb93
Michael Milroy <mdmilroy@gmail.com>
2020-04-16 19:02:36 -0400

    adding Categories

---
+++ ElevenNote.Services/CategoryService.cs
@@ -0,0 +1,58 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class CategoryService
+    {
+        private readonly Guid _userId;
+
+        public CategoryService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateCategory(CategoryCreate model)
+        {
+            var entity =
+                new Category()
+                {
+                    OwnerId = _userId,
+                    Name = model.Name,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CategoryListItem> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Categories
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
